5e401ea5609bb2341c9454ce362ed3e825a85ad2
Genevieve Masioni <masionigenevieve@gmail.com>
2019-03-24 16:39:30 +0100

    Implement new feature : activate widgets areas

--- wordpress/footer.php
+++ wordpress/footer.php
@@ -2,60 +2,31 @@
 <footer>
   <section id="a-propos" class="section widgets widget-section-1">
     <section class="column col-1 text-col">
-      <div class="widget">
-        <h2 class="widget-title">Qui suis-je ?</h2>
-        <p>Développeuse web indépendante, je travaille en étroite collaboration
-          avec des graphistes spécialisés dans le numérique et des artistes pour
-          concevoir des sites web et des applications mobiles de qualité professionnelle.
-        </p>
-        <p>Possi dolorporum harum amenihi llabor ariscidescia nobis sintiae rferio. Nisti doluptas es sundus intia preicte cum nit, sunt autem harcimp orrundae voloribus, eum, sus.
-          Untem. Hit eicidit atendio nserume est fuga. Nequost omnisseque vendit haritati to que verunt.
-          Xerunt quia deligent pa pererunt veni rem fugitibea volupitiore pari voluptium ero doluptat.
-
-          Sam lacest reicia aut quo magnit utat dollabo. Ullendebis et que nus, optum quat dolupta ecullore dit, eum restiscipit dolorum et quis veritatur?
-          Conectus. Sa vitas moluptatur, ut enecus sum quam expla velesciet aditiniet libusaniam
-        </p>
-      </div>
+      <?php if(is_active_sidebar('footer-widget-1')) : ?>
+        <?php dynamic_sidebar('footer-widget-1'); ?>
+      <?php endif; ?>
     </section>
-    <section class="column col-2 img-col">
-      <div class="widget">
-        <img src="<?php echo get_bloginfo('template_url'); ?>/images/mug.jpg" alt="Geneviève Masioni">
-      </div>
+    <section class="column col-2 text-col profile-resume">
+      <?php if(is_active_sidebar('footer-widget-2')) : ?>
+        <?php dynamic_sidebar('footer-widget-2'); ?>
+    <?php endif; ?>
     </section>
     <section class="column col-3 text-col accent">
-      <div class="widget">
-        <h2 class="widget-title">Réseaux</h2>
-        <ul>
-          <li><a href="#">Github</a></li>
-          <li><a href="#">Instagram</a></li>
-          <li><a href="#">Youtube</a></li>
-          <li><a href="#">Boutique</a></li>
-        </ul>
-        <p class="copyright">&copy; 2019 &#8212; Codé avec passion par Geneviève Masioni</p>
-      </div>
+      <?php if(is_active_sidebar('footer-widget-3')) : ?>
+        <?php dynamic_sidebar('footer-widget-3'); ?>
+      <?php endif; ?>
     </section>
   </section>
   <section id="contact" class="section widgets widget-section-2">
     <section class="column col-1 text-col">
-      <div class="widget">
-        <h2 class="widget-title">Travaillons ensemble</h2>
-        <form action="index.html" method="post">
-          <input type="text" placeholder="Nom">
-          <input type="email" placeholder="Email">
-          <select class="">
-            <option>Appel d'offre</option>
-            <option>Partenariat</option>
-            <option>Autre</option>
-          </select>
-          <textarea name="message" rows="8" placeholder="Message"></textarea>
-        </form>
-      </div>
+      <?php if(is_active_sidebar('footer-widget-4')) : ?>
+        <?php dynamic_sidebar('footer-widget-4'); ?>
+      <?php endif; ?>
     </section>
     <section class="column col-2 text-col accent">
-      <div class="widget">
-        <h2 class="widget-title">Témoignages</h2>
-        <p>[testimonials]</p>
-      </div>
+      <?php if(is_active_sidebar('footer-widget-5')) : ?>
+        <?php dynamic_sidebar('footer-widget-5'); ?>
+      <?php endif; ?>
     </section>
   </section>
 </footer>
--- wordpress/functions.php
+++ wordpress/functions.php
@@ -17,9 +17,69 @@ function darkfolio_setup() {
 
 add_action('after_setup_theme', 'darkfolio_setup');
 
+
+/**
+ * Filter the_content() method to not wrap <img> tags with <p> tag
+ * @author Genevieve Masioni <genevievemasioni.com>
+ */
 function filter_ptags_on_images($content){
   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
 
 add_filter('the_content', 'filter_ptags_on_images');
+
+/**
+ * Initialize widgets location
+ * @author Genevieve Masioni <genevievemasioni.com>
+ */
+function init_widgets($id) {
+  register_sidebar(array(
+    'name'    => 'Footer widget 1',
+    'id'      => 'footer-widget-1',
+    'before_widget' => '<div class="widget">',
+    'after_widget' => '</div>',
+    'before-title' => '<h2 class="widget-title">',
+    'after_title' => '</h2>'
+  ));
+
+  register_sidebar(array(
+    'name'    => 'Footer widget 2',
+    'id'      => 'footer-widget-2',
+    'before_widget' => '<div class="widget">',
+    'after_widget' => '</div>',
+    'before-title' => '<h2 class="widget-title">',
+    'after_title' => '</h2>'
+  ));
+
+  register_sidebar(array(
+    'name'    => 'Footer widget 3',
+    'id'      => 'footer-widget-3',
+    'before_widget' => '<div class="widget">',
+    'after_widget' => '</div>',
+    'before-title' => '<h2 class="widget-title">',
+    'after_title' => '</h2>'
+  ));
+
+  register_sidebar(array(
+    'name'    => 'Footer widget 4',
+    'id'      => 'footer-widget-4',
+    'before_widget' => '<div class="widget">',
+    'after_widget' => '</div>',
+    'before-title' => '<h2 class="widget-title">',
+    'after_title' => '</h2>'
+  ));
+
+  register_sidebar(array(
+    'name'    => 'Footer widget 5',
+    'id'      => 'footer-widget-5',
+    'before_widget' => '<div class="widget">',
+    'after_widget' => '</div>',
+    'before-title' => '<h2 class="widget-title">',
+    'after_title' => '</h2>'
+  ));
+
+}
+
+// Widget
+add_action('widgets_init', 'init_widgets');
 ?>
